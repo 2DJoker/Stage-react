d8b5bb5350044bea17168de6900343e81f0c1699
Benzo! <109986015+2DJoker@users.noreply.github.com>
2024-07-09 17:43:31 -0600

    Добавил поиск, наведение на карточку товара с тенью, Лайки и дизлайки на карточки

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 function App() {
-  return <div className="wrapper">
+  
 
+  return <div className="wrapper">
     <header>
       <div className="headerleft">
       <img width={73} height={70} src="/img/cat-logo.jpg" alt="loho"/>
@@ -19,12 +20,21 @@ function App() {
         </li>
       </ul>
     </header>
+
   <div className="contentpages">
-    <h1>Каталог</h1>
+   <div className="wrapper-search">
+   <h1>Каталог</h1>
+    <div className="search-block">
+    <img height={15} width={15} src="/img/search.png" alt="search-pick"/>
+    <input placeholder="Search..."/>
+     </div>
+   </div>
     
   <div className="Sneakers">
-    
   <div className="card"> 
+   <div className="heart">
+    <img height={25} width={25} src="/img/heart.svg" alt="heart"/>
+   </div>
     <img width={150} height={160} src="/img/items/force.png" alt="goods"/>
     <h5>Yeezy Boost 350 kakashki</h5>
       <div className="CardButt">
