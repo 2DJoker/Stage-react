9b22b8ff1d70e0f34c5d1543da5e7ba40083df9d
Benzo! <109986015+2DJoker@users.noreply.github.com>
2024-07-09 06:55:50 -0600

    sneakers catalog

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
 
     <header>
       <div className="headerleft">
-      <img width={73} height={70} src="/img/cat-logo.jpg"/>
+      <img width={73} height={70} src="/img/cat-logo.jpg" alt="loho"/>
       <div className="headerinfo"> 
         <h3>Stage!</h3>
         <p>Sneakers store</p>
@@ -11,18 +11,71 @@ function App() {
       </div>
       <ul className="headerRight">
         <li>
-          <img width={40} height={40} src="/img/basket.png"/>
+          <img width={40} height={40} src="/img/basket.png" alt="basket"/>
         </li>
         <span>137 рублей</span>
         <li>
-          <img width={29} height={29} src="/img/user.png"/> 
+          <img width={29} height={29} src="/img/user.png" alt="logo"/> 
         </li>
       </ul>
     </header>
   <div className="contentpages">
     <h1>Каталог</h1>
-    ........
+    
+  <div className="Sneakers">
+    
+  <div className="card"> 
+    <img width={150} height={160} src="/img/items/force.png" alt="goods"/>
+    <h5>Yeezy Boost 350 kakashki</h5>
+      <div className="CardButt">
+        <div className="CardPrice">
+          <span>Price:</span>
+          <b>139.99 $</b>
+        </div>
+        <button className="button">
+          <img width={9} height={9} src="/img/plus.png" alt="plus"/>
+      </button>
+     </div>
+      </div>
+      <div className="card"> 
+    <img width={210} height={180} src="/img/items/yeezy4.jpeg" alt="goods"/>
+    <h5>Yeezy Boost 350 kakashki</h5>
+      <div className="CardButt">
+        <div className="CardPrice">
+          <span>Price:</span>
+          <b>139.99 $</b>
+        </div>
+        <button className="button">
+          <img width={9} height={9} src="/img/plus.png" alt="plus"/>
+      </button>
+     </div>
+      </div> <div className="card"> 
+    <img width={150} height={160} src="/img/items/force.png" alt="goods"/>
+    <h5>Yeezy Boost 350 kakashki</h5>
+      <div className="CardButt">
+        <div className="CardPrice">
+          <span>Price:</span>
+          <b>139.99 $</b>
+        </div>
+        <button className="button">
+          <img width={9} height={9} src="/img/plus.png" alt="plus"/>
+      </button>
+     </div>
+      </div> <div className="card"> 
+    <img width={210} height={180} src="/img/items/yeezy4.jpeg" alt="goods"/>
+    <h5>Yeezy Boost 350 kakashki</h5>
+      <div className="CardButt">
+        <div className="CardPrice">
+          <span>Price:</span>
+          <b>139.99 $</b>
+        </div>
+        <button className="button">
+          <img width={9} height={9} src="/img/plus.png" alt="plus"/>
+      </button>
+     </div>
+      </div>
   </div>
+    </div>
   </div>;
 }
 
